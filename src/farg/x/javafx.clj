995fb1000f62e643c1ffd01a0f4f802cf6e1824b
Ben Kovitz <bkovitz@indiana.edu>
2018-06-08 14:07:49 -0400

    DragNodes
    Yes, you can put event handlers on shapes in JavaFX (even from
    Clojure).
    
    Three bits of strangeness:
    
    1. It's necessary to pass .addAll the result of calling into-array
    rather than passing arguments directory. See mkstage-dragnodes in
    javafx.clj.
    
    2. mkstage-dragnodes must run in the FX thread even though it
    doesn't affect the screen. So, you have to run the demonstration
    like this:
    
      (def s (run-now (mkstage-dragnodes)))
      (go s)
    
    3. The translucent BLUE and GREEN hues returned from .deriveColor are
    slightly different from those in the native Java version of the
    application (in the javafx/ directory).

--- src/farg/x/javafx.clj
+++ src/farg/x/javafx.clj
@@ -5,9 +5,12 @@
             [clojure.tools.trace :refer [deftrace] :as trace]
             [clojure.pprint :refer [pprint]])
   (:import [javafx.event ActionEvent EventHandler]
-           [javafx.scene Scene SceneBuilder]
+           [javafx.scene Scene SceneBuilder Group Node]
+           [javafx.scene.canvas Canvas GraphicsContext]
            [javafx.scene.control Button ButtonBuilder]
-           [javafx.scene.layout VBoxBuilder StackPane]
+           [javafx.scene.layout Pane VBoxBuilder StackPane]
+           [javafx.scene.paint Color]
+           [javafx.scene.shape Circle Line]
            [javafx.stage Stage StageBuilder]))
 
 ;;; Code largely taken from https://gist.github.com/zilti/6286307
@@ -75,10 +78,10 @@
   (let [btn (doto (new Button)
               (.setText "Hello, World!")
               (.setOnAction (event-handler [_] (println "Hello, World!"))))
-        root (new StackPane)
-        primary-stage (new Stage)]
-    (.add (.getChildren root) btn)
-    (.setScene primary-stage (new Scene root 300 250))
+        root (doto (new StackPane)
+               (.. getChildren (add btn)))
+        primary-stage (doto (new Stage)
+                        (.setScene (new Scene root 300 250)))]
     primary-stage))
 
 (def stage (atom nil))
@@ -88,5 +91,77 @@
     (.close old-st))
   (doto new-st .show .toFront))
 
-(defn go []
-  (run-now (swap! stage new-stage! (mkstage2))))
+(defn go
+ ([]
+  (go (mkstage2)))
+ ([st]
+  (run-now (swap! stage new-stage! st))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn circle? [x]
+  (isa? x Circle))
+
+(defn make-draggable [node]
+  (let [originals (atom {:scene-x nil, :scene-y nil, :translate-x nil,
+                         :translate-y nil})]
+    (doto node
+      (.setOnMousePressed
+        (event-handler [e]
+          (let [src (.getSource e)]
+            (swap! originals assoc
+              :scene-x (.getSceneX e)
+              :scene-y (.getSceneY e)
+              :translate-x (if (circle? src)
+                             (.getCenterX src)
+                             (.getTranslateX src))
+              :translate-y (if (circle? src)
+                             (.getCenterY src)
+                             (.getTranslateY src))))))
+      (.setOnMouseDragged
+        (event-handler [e]
+          (let [src (.getSource e)
+                offsetX (- (.getSceneX e) (:scene-x @originals))
+                offsetY (- (.getSceneY e) (:scene-y @originals))
+                newX (+ (:translate-x @originals) offsetX)
+                newY (+ (:translate-y @originals) offsetY)]
+            (if (circle? src)
+              (doto src
+                (.setCenterX newX)
+                (.setCenterY newY))
+              (doto src
+                (.setTranslateX newX)
+                (.setTranslateY newY)))))))))
+
+(defn mkstage-dragnodes
+  "Makes the Stage for the DragNodes app from
+  https://stackoverflow.com/a/28000358/1393162"
+  []
+  (let [canvas (new Canvas 300 300)
+        gc (doto (.getGraphicsContext2D canvas)
+             (.setStroke Color/RED)
+             (.strokeRoundRect 10 10 230 230 10 10))
+        circle1 (doto (new Circle 50)
+                  (.setStroke Color/GREEN)
+                  (.setFill (.deriveColor Color/GREEN 1 1 1 0.7))
+                  (.relocate 100 100)
+                  make-draggable)
+        circle2 (doto (new Circle 50)
+                  (.setStroke Color/BLUE)
+                  (.setFill (.deriveColor Color/BLUE 1 1 1 0.7))
+                  (.relocate 200 200)
+                  make-draggable)
+        line (doto (new Line (.getLayoutX circle1)
+                             (.getLayoutY circle1)
+                             (.getLayoutX circle2)
+                             (.getLayoutY circle2))
+               (.setStrokeWidth 20)
+               make-draggable)
+        overlay (doto (new Pane) 
+                  (.. getChildren (addAll
+                    (into-array Node [circle1 circle2 line]))))
+        root (doto (new Group)
+               (.. getChildren (addAll (into-array Node [canvas overlay]))))
+        stage (doto (new Stage)
+                (.setScene (new Scene root 800 600)))]
+    stage))
